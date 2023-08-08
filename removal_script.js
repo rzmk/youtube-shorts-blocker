2457703b86e50b0848595dcccc8123ef59069630
rzmk <mueez25@gmail.com>
2023-08-07 23:17:20 -0400

    :art: Allow blocking Shorts sidebar tab on /* route

--- removal_script.js
+++ removal_script.js
@@ -1,8 +1,10 @@
 // Wait for the page to finish loading
 window.addEventListener("load", () => {
-  // Find the anchor tag with the title "Shorts"
-  const anchor = document.querySelector('a[title="Shorts"]');
+    // Find the anchor tag with the title "Shorts"
+    const anchor = document.querySelector('a[title="Shorts"]');
 
-  // If the anchor exists, remove it from the page
-  if (anchor) anchor.remove();
+    // If the anchor's parent exists, remove it from the page
+    if (anchor.parentElement) anchor.parentElement.remove();
+    // Otherwise, if the anchor exists, remove it from the page
+    else if (anchor) anchor.remove();
 });
